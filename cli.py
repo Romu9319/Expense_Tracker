9510bb580619a3c1d09160837daab0abefabcbf7
unknown <danielerm1510@gmail.com>
2025-03-11 21:39:05 +0100

    update command added

--- cli.py
+++ cli.py
@@ -7,7 +7,7 @@ import datetime
 def cli():
     pass
 
-#CAMBIAR LOS ARGUMENT POR OPTION, VER COMO FUNCIONA CORRECTAMENTE EL --HELP
+
 # Add new Expense
 @cli.command()
 @click.argument('description', required=True)
@@ -41,6 +41,52 @@ def add(ctx, description, amount, date):
     data.append(new_expense)
     json_manager.add_expenses(data)
     click.echo(f"New expense with id {expense_id} has been added")
+
+
+# Update Expense by id
+@cli.command()
+@click.argument('id', type=str) #ver por que puse tipo id str y no int
+@click.argument('description', type=str)
+@click.argument('amount', type=int)
+@click.option('--date', help="Modify the spending date")
+@click.pass_context
+def update(ctx, id, description, amount, date):
+    """
+    Actualiza los datos de los gastos
+    """
+    if not description.strip():
+        ctx.fail("Task description is required")
+
+    try:
+        expense_id = int(id)
+    except ValueError:
+        ctx.fail("A numeric ID is required, try again")
+
+    data = json_manager.read_json()
+    expense_found = False
+
+    for expense in data:
+        if expense["id"] == expense_id:
+            if description is not None:
+                expense["description"] = description
+            if amount is not None:
+                expense["amount"] = amount
+            if date is not None:
+                 try:
+                    datetime.datetime.strptime(date, "%Y-%m-%d")
+                    expense["date"] = date
+                 except ValueError:
+                    ctx.fail("Invalid date format! Please use YYYY-MM-DD.")
+            expense_found = True
+            break
+        
+    if expense_found:
+        json_manager.add_expenses(data)
+        click.echo(f"Expense with id {expense_id} has been updated")
+    
+    if not expense_found:
+        click.echo(f"Expense with id {expense_id} not found")
+
 
 
 if __name__ == '__main__':
